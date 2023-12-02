c79ddd7670661cd9cfa9303cbb1474e9a0f9e847
s-a-mcdonnell <150218525+s-a-mcdonnell@users.noreply.github.com>
2023-12-01 19:00:26 -0500

    Draw map
    Not scaled at all

--- CompostFinder.java
+++ CompostFinder.java
@@ -1,5 +1,8 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 public class CompostFinder extends JPanel {
     public static final int WIDTH = 1024;
@@ -8,13 +11,22 @@ public class CompostFinder extends JPanel {
     public static final double CENTERX = WIDTH / 2.0;
     public static final double CENTERY = HEIGHT / 2.0;
 
+    BufferedImage image;
+
     public CompostFinder() {
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+
+        try {
+            this.image = ImageIO.read(new File("C:\\Users\\skyle\\Downloads\\ACHigherQ.png"));
+        } catch (Exception e) {
+            System.err.println("Error");
+        }
+
     }
 
     public void Go() {
         while (true) {
-            //__ update
+            //TODO: Write update methods for whatever needs to be updated
 
             repaint();
             try {
@@ -35,7 +47,7 @@ public class CompostFinder extends JPanel {
     }
 
     public void paintComponent(Graphics g) {
-        g.setColor(Color.RED);
-        g.fillRect(100, 100, 100, 100);
+        //TODO: Scale
+        g.drawImage(this.image, 0, 0, null);
     }
 }

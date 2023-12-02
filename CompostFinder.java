7ad645dcaec132710257f5f7da7363ebae631f27
s-a-mcdonnell <150218525+s-a-mcdonnell@users.noreply.github.com>
2023-12-01 19:21:14 -0500

    Scale image correctly
    Using Skyler path

--- CompostFinder.java
+++ CompostFinder.java
@@ -18,6 +18,12 @@ public class CompostFinder extends JPanel {
 
         try {
             this.image = ImageIO.read(new File("C:\\Users\\skyle\\Downloads\\ACHigherQ.png"));
+            int w = this.image.getWidth();
+            int h = this.image.getHeight();
+            double proportion = ((double) w) / (double) h;
+
+
+            this.image = toBufferedImage(image.getScaledInstance((int) (HEIGHT * proportion), HEIGHT, java.awt.Image.SCALE_SMOOTH));
         } catch (Exception e) {
             System.err.println("Error");
         }
@@ -49,5 +55,23 @@ public class CompostFinder extends JPanel {
     public void paintComponent(Graphics g) {
         //TODO: Scale
         g.drawImage(this.image, 0, 0, null);
+    }
+
+    // From https://stackoverflow.com/questions/13605248/java-converting-image-to-bufferedimage
+    public static BufferedImage toBufferedImage(Image img) {
+        if (img instanceof BufferedImage) {
+            return (BufferedImage) img;
+        }
+
+        // Create a buffered image with transparency
+        BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+
+        // Draw the image on to the buffered image
+        Graphics2D bGr = bimage.createGraphics();
+        bGr.drawImage(img, 0, 0, null);
+        bGr.dispose();
+
+        // Return the buffered image
+        return bimage;
     }
 }
